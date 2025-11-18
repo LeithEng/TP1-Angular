b4a9d087ed4effbae8d5c8406eacf068e7b52d94
LeithEng <leithengazzou@gmail.com>
2025-11-18 15:41:20 +0100

    feat: added product fetching to rxjs branch

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,24 +1,53 @@
-import { Component } from "@angular/core";
-import {
-  BehaviorSubject,
-  Observable,
-  concatMap,
-  map,
-  takeWhile,
-  scan,
-} from "rxjs";
-import { Product } from "./dto/product.dto";
-import { ProductService } from "./services/product.service";
-import { Settings } from "./dto/product-settings.dto";
+import { Component } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { concatMap, map, scan, startWith, takeWhile } from 'rxjs/operators';
+import { Product } from './dto/product.dto';
+import { ProductService } from './services/product.service';
+import { Settings } from './dto/product-settings.dto';
 
 @Component({
-  selector: "app-products",
-  templateUrl: "./products.component.html",
-  styleUrls: ["./products.component.css"],
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
-  /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
-  /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
   products$!: Observable<Product[]>;
-  constructor() {}
+  hasMore$!: Observable<boolean>;
+
+  private loadMore$ = new Subject<void>();
+  private readonly pageSize = 12;
+
+  constructor(private productService: ProductService) {
+    const pageIndex$ = this.loadMore$.pipe(
+      startWith(null as unknown as void),
+      scan((acc) => acc + 1, -1)
+    );
+
+    const accumulation$ = pageIndex$.pipe(
+      map(
+        (pageIndex) =>
+          ({
+            limit: this.pageSize,
+            skip: pageIndex * this.pageSize,
+          } as Settings)
+      ),
+      concatMap((setting) => this.productService.getProducts(setting)),
+      scan(
+        (acc, resp) => ({
+          products: [...acc.products, ...resp.products],
+          total: resp.total,
+        }),
+        { products: [] as Product[], total: 0 }
+      ),
+      takeWhile((acc) => acc.products.length < acc.total, true)
+    );
+    this.products$ = accumulation$.pipe(map((acc) => acc.products));
+    this.hasMore$ = accumulation$.pipe(
+      map((acc) => acc.products.length < acc.total)
+    );
+  }
+
+  loadMoreProducts() {
+    this.loadMore$.next();
+  }
 }
